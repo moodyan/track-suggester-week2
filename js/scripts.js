37d41ecb5eefa435d284c8bc9149524a11b622cf
Alyssa Moody <alyssanicholemoody@gmail.com>
2017-04-28 11:20:48 -0700

    Fixed siblings.hide argument

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("form#quiz").submit(function(event){
 
-    var nameInput = $("#name").val();
+    var nameInput = $("input#name").val();
     var devInput = $("select#development").val();
     var companyInput = $("select#company").val();
     var taskInput = $("select#task").val();
@@ -10,106 +10,109 @@ $(document).ready(function(){
 
     if (devInput === "front-end") {
       $("#answer-css").show();
+      $("#answer-css").siblings().hide();
     } else {
         if (companyInput === "company-csharp" && taskInput === "task-csharp" && attInput === "attribute-phpcsharp" && systemInput === "system-csharp") {
           $("#answer-csharp").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-csharp").siblings().hide();
         }
         if (companyInput === "company-csharp" && taskInput === "task-ruby" && attInput === "attribute-phpcsharp" && systemInput === "system-csharp") {
           $("#answer-csharp").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-csharp").siblings().hide();
         }
         if (companyInput === "company-csharp" && taskInput === "task-php" && attInput === "attribute-phpcsharp" && systemInput === "system-csharp") {
           $("#answer-csharp").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-csharp").siblings().hide();
         }
         if (companyInput === "company-csharp" && taskInput === "task-csharp" && attInput === "attribute-ruby" && systemInput === "system-csharp") {
           $("#answer-csharp").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-csharp").siblings().hide();
         }
         if (companyInput === "company-csharp" && taskInput === "task-ruby" && attInput === "attribute-ruby" && systemInput === "system-csharp") {
           $("#answer-ruby").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-ruby").siblings().hide();
         }
         if (companyInput === "company-csharp" && taskInput === "task-php" && attInput === "attribute-ruby" && systemInput === "system-csharp") {
           $("#answer-csharp").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-csharp").siblings().hide();
         }
         if (companyInput === "company-csharp" && taskInput === "task-csharp" && attInput === "attribute-phpcsharp" && systemInput === "system-phpruby") {
           $("#answer-csharp").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-csharp").siblings().hide();
         }
         if (companyInput === "company-csharp" && taskInput === "task-ruby" && attInput === "attribute-phpcsharp" && systemInput === "system-phpruby") {
           $("#answer-ruby").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-ruby").siblings().hide();
         }
 
         if (companyInput === "company-csharp" && taskInput === "task-php" && attInput === "attribute-phpcsharp" && systemInput === "system-phpruby") {
           $("#answer-php").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-php").siblings().hide();
         }
         if (companyInput === "company-csharp" && taskInput === "task-csharp" && attInput === "attribute-ruby" && systemInput === "system-phpruby") {
           $("#answer-csharp").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-csharp").siblings().hide();
         }
         if (companyInput === "company-csharp" && taskInput === "task-ruby" && attInput === "attribute-ruby" && systemInput === "system-phpruby") {
           $("#answer-ruby").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-ruby").siblings().hide();
         }
         if (companyInput === "company-csharp" && taskInput === "task-php" && attInput === "attribute-ruby" && systemInput === "system-phpruby") {
           $("#answer-php").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-php").siblings().hide();
         }
         if (companyInput === "company-rubyphp" && taskInput === "task-csharp" && attInput === "attribute-phpcsharp" && systemInput === "system-csharp") {
           $("#answer-csharp").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-csharp").siblings().hide();
         }
         if (companyInput === "company-rubyphp" && taskInput === "task-ruby" && attInput === "attribute-phpcsharp" && systemInput === "system-csharp") {
           $("#answer-ruby").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-ruby").siblings().hide();
         }
         if (companyInput === "company-rubyphp" && taskInput === "task-php" && attInput === "attribute-phpcsharp" && systemInput === "system-csharp") {
           $("#answer-php").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-php").siblings().hide();
         }
         if (companyInput === "company-rubyphp" && taskInput === "task-csharp" && attInput === "attribute-ruby" && systemInput === "system-csharp") {
           $("#answer-csharp").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-csharp").siblings().hide();
         }
         if (companyInput === "company-rubyphp" && taskInput === "task-ruby" && attInput === "attribute-ruby" && systemInput === "system-csharp") {
           $("#answer-ruby").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-ruby").siblings().hide();
         }
         if (companyInput === "company-rubyphp" && taskInput === "task-php" && attInput === "attribute-ruby" && systemInput === "system-csharp") {
           $("#answer-php").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-php").siblings().hide();
         }
         if (companyInput === "company-rubyphp" && taskInput === "task-csharp" && attInput === "attribute-phpcsharp" && systemInput === "system-phpruby") {
           $("#answer-php").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-php").siblings().hide();
         }
         if (companyInput === "company-rubyphp" && taskInput === "task-ruby" && attInput === "attribute-phpcsharp" && systemInput === "system-phpruby") {
           $("#answer-ruby").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-ruby").siblings().hide();
         }
         if (companyInput === "company-rubyphp" && taskInput === "task-php" && attInput === "attribute-phpcsharp" && systemInput === "system-phpruby") {
           $("#answer-php").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-php").siblings().hide();
         }
         if (companyInput === "company-rubyphp" && taskInput === "task-csharp" && attInput === "attribute-ruby" && systemInput === "system-phpruby") {
           $("#answer-ruby").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-ruby").siblings().hide();
         }
         if (companyInput === "company-rubyphp" && taskInput === "task-ruby" && attInput === "attribute-ruby" && systemInput === "system-phpruby") {
           $("#answer-ruby").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-ruby").siblings().hide();
         }
         if (companyInput === "company-rubyphp" && taskInput === "task-php" && attInput === "attribute-ruby" && systemInput === "system-phpruby") {
           $("#answer-php").show();
-          // $(".answer-csharp").siblings("#answer-section").hide();
+          $("#answer-php").siblings().hide();
         }
     }
 
+    $("#name").text(nameInput);
+    
   event.preventDefault();
   });
 });
